337083401f48adc81653b15fc1eb102d95749c92
clodverne <clodverne@gmail.com>
2017-05-16 18:22:38 +0300

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,6 @@
+$('document').ready(function() {
+	var options = { videoId: 'Riw1jQ0wdPk', start: 3 };
+	$('.wrapper').tubular(options);
+	// f-UGhWj1xww cool sepia hd
+	// 49SKbS7Xwf4 beautiful barn sepia
+});
